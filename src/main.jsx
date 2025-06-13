851883a2a0127c4675059ba01b57781526c71bb0
93372377 <142664513+93372377@users.noreply.github.com>
2025-06-13 23:12:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { MsalProvider } from '@azure/msal-react';
 import { msalInstance } from './authConfig';
-import { MsalAuthenticationTemplate, MsalRedirectComponent } from '@azure/msal-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,7 +10,6 @@ root.render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
       <App />
-      <MsalRedirectComponent /> {/* Important: handles loginRedirect result */}
     </MsalProvider>
   </React.StrictMode>
 );
