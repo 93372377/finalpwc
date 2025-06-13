7f023368cbaf1ebd0bc57d1cce3742bc906aeb70
93372377 <142664513+93372377@users.noreply.github.com>
2025-06-14 01:42:53 +0200

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,8 +2,8 @@ import { PublicClientApplication } from '@azure/msal-browser';
 
 export const msalConfig = {
   auth: {
-    clientId: '4d661288-c66e-45a8-a8a3-b76b795be7f9', // your App (client) ID
-    authority: 'https://login.microsoftonline.com/a00de4ec-48a8-43a6-be74-e31274e2060d', // your Directory (tenant) ID
+    clientId: '4d661288-c66e-45a8-a8a3-b76b795be7f9', // App (client) ID
+    authority: 'https://login.microsoftonline.com/a00de4ec-48a8-43a6-be74-e31274e2060d', // Tenant ID
     redirectUri: 'https://finalfinalpwc5.netlify.app'
   },
   cache: {
@@ -13,9 +13,12 @@ export const msalConfig = {
 };
 
 export const loginRequest = {
-  scopes: ['User.Read'],
-  prompt: 'login' // force credential entry every time
+  scopes: [
+    'User.Read',
+    'Files.ReadWrite.All',
+    'Sites.ReadWrite.All'
+  ],
+  prompt: 'login'
 };
 
 export const msalInstance = new PublicClientApplication(msalConfig);
-
