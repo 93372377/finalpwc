8f5df29c13284ee4eb7310682f257f453104fd1a
93372377 <142664513+93372377@users.noreply.github.com>
2025-06-16 14:20:50 +0200

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: '4d661288-c66e-45a8-a8a3-b76b795be7f9', // App (client) ID
     authority: 'https://login.microsoftonline.com/a00de4ec-48a8-43a6-be74-e31274e2060d', // Tenant ID
-    redirectUri: 'https://finalfinalpwc23.netlify.app'
+    redirectUri: 'https://finalfinalpwc24.netlify.app'
   },
   cache: {
     cacheLocation: 'localStorage',
